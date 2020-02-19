5183a4baf991829759cab08b4b8063620b858f33
ladan <pishili@users.noreply.github.com>
2020-02-18 22:03:50 -0800

    used useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 
@@ -22,11 +22,10 @@ const days = [
   },
 ];
 
-const setDay = (day) => {
-  console.log(day);
-}
 
 export default function Application(props) {
+  const [currentDayName, setDay] = useState("Monday")
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -39,7 +38,7 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
+            day={currentDayName}
             setDay={setDay}
           />
         </nav>
