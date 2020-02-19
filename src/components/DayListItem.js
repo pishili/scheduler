ad81f01765ab98e6689b3fae4d58cd18890fc637
ladan <pishili@users.noreply.github.com>
2020-02-18 18:18:53 -0800

    fixed day list items and listing days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,8 +3,6 @@ import "./DayListItem.scss";
 
 var classNames = require('classnames');
 
-// format the props.spots to make our tests pass.
-// format the props.spots
 const formatSpots = (spots) => {
 	if (spots === 0) {
 		return "no spots remaining";
@@ -17,20 +15,15 @@ const formatSpots = (spots) => {
 	}
 };
 
-
 export default function DayListItem(props) {
-
-	
-
 	let dayClass = classNames(["day-list__item",
 							  { "day-list__item--selected": props.selected,
 							    "day-list__item--full": props.spots === 0 }]);
-	
-				
+			
 	return (
-	<li onClick={() => props.setDay(props.name)}>
-	   <h2 className={dayClass}>{props.name}</h2>
-	   <h3 className={dayClass}>{formatSpots(props.spots)}</h3>
+	<li className={dayClass} onClick={() => props.setDay(props.name)}>
+	   <h2>{props.name}</h2>
+	   <h3>{formatSpots(props.spots)}</h3>
     </li>
   );
 }
