b84ac8d7e28bbde184aedb99297e7a5f9e120095
ladan <pishili@users.noreply.github.com>
2020-02-19 21:08:45 -0800

    fixes InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "styles/InterviewerListItem.scss";
+import { storiesOf } from "@storybook/react";
 
 var classNames = require('classnames');
 
 export default function InterviewListItem(props) {
+    let className = classNames(
+        ["interviewers__item",
+            {"interviewers__item--selected &-image": props.selected
+            }
+        ]
+    );
+
+    let name = "";
+    if (props.selected) {
+        name = props.name
+    }
+
     return (
-    <li className="interviewers__item">
+    <li className={className}>
         <img
-            className="interviewers__item-image"
-            src="https://i.imgur.com/LpaY82x.png"
-            alt="Sylvia Palmer"
+            className={className}
+            src={props.avatar}
+            alt={props.name}
         />
-        Sylvia Palmer
+        {name}
     </li>
     )
 }
+
