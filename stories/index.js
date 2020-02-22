85756f6fb07d5b655b7a4bffeeb07f965c15a506
ladan <pishili@users.noreply.github.com>
2020-02-22 13:46:09 -0800

    added appointment stories to index

--- stories/index.js
+++ stories/index.js
@@ -189,3 +189,14 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />)
   )
+ 
+  storiesOf("Appointment", module)
+  .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => (
+    <Appointment
+      time = "12pm"
+     />)
+    );
