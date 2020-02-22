53c37632894bc54639f06977c811389cb6b1e837
ladan <pishili@users.noreply.github.com>
2020-02-22 12:19:29 -0800

    Fix onCLick in Confirm

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Button from "components/Button";
-import "components/Appointment/styles.scss";
+
+var classNames = require('classnames');
 
 
 export default function Confirm(props) {
 
+    const { message, onCancel, onConfirm } = props;
+
     return (
         <main className="appointment__card appointment__card--confirm">
-            <h1 className="text--semi-bold">{props.message}</h1>
+            <h1 className="text--semi-bold">{message}</h1>
             <section className="appointment__actions">
-                <Button danger>{props.onCancel}</Button>
-                <Button danger>{props.onConfirm}</Button>
+                <Button onClick={onCancel} danger>Cancel</Button>
+                <Button onClick={onConfirm} danger>Confirm</Button>
             </section>
         </main>
 
