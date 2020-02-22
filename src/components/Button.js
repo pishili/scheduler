f411f7b0f730fe8b6a52d25191f3044dd3eb3104
ladan <pishili@users.noreply.github.com>
2020-02-22 12:18:32 -0800

    Fix button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,24 @@
 import React from "react";
 
-import "styles/Button.scss";
+import "./Button.scss";
 import classnames from "classnames";
 
 
 export default function Button(props) {
+
+   const { confirm, danger, onClick, disabled, children } = props
+
    const buttonClass = classnames("button", {
-      "button--confirm": props.confirm,
-      "button--danger": props.danger
+      "button--confirm": confirm,
+      "button--danger": danger
    });
 
    return (<button
       className={buttonClass}
-      onClick={props.onClick}
-      disabled={props.disabled}
+      onClick={onClick}
+      disabled={disabled}
    >
-      {props.children}
+      {children}
    </button>
    );
 }
