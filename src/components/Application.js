ecc52dbad035661c08b0b4b5e98faffe9001e84e
ladan <pishili@users.noreply.github.com>
2020-02-23 12:15:19 -0800

    Uses axios to fetch Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,64 +5,9 @@ import "styles/Application.scss"
 
 import DayList from "components/Day/DayList"
 import Appointment from "components/Appointment"
+import { getAppointmentsForDay } from "helpers/selectors"
 
 const axios = require('axios');
-
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "6pm",
-    interview: {
-      student: "Jennifer Aniston",
-      interviewer: {
-        id: 1,
-        name: "Erik Nel",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "11am",
-    interview: {
-      student: "Lynda Sabori",
-      interviewer: {
-        id: 1,
-        name: "Charles Hughes",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "3pm",
-    interview: {
-      student: "Leonardo Ebrol",
-      interviewer: {
-        id: 1,
-        name: "Shams Yaroyi",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
@@ -70,11 +15,27 @@ export default function Application(props) {
   // to the our Application component. It can be initialized as an empty array.
   const [days, setDays] = useState([]);
 
+  const [appointments, setAppointments] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:8001/api/days").then((response) => {
       setDays(response.data);
     });
   }, [])
+
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/appointments").then((response) => {
+      // console.log(Object.values(response.data));
+      const appointments = getAppointmentsForDay(
+        {
+          days: days,
+          appointments: response.data
+        },
+        day);
+      setAppointments(appointments);
+    });
+  }, [day])
 
 
   return (
