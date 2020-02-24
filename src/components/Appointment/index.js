c011d1c9421672b68140e9d230f2097b736d82eb
ladan <pishili@users.noreply.github.com>
2020-02-23 23:15:25 -0800

    Adds a few transitions in Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,11 +5,13 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Confirm from "components/Appointment/Confirm";
 
 import useVisualMode from "hooks/useVisualMode";
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
+const EMPTY = "EMPTY"
+const SHOW = "SHOW"
+const CONFIRM = "CONFIRM"
 
 export default function Appointment(props) {
   const { time, interview } = props;
@@ -25,7 +27,12 @@ export default function Appointment(props) {
       case SHOW:
         return <Show
           student={interview.student}
-          interviewer={interview.interviewer} />
+          interviewer={interview.interviewer}
+          onDelete={() => transition(CONFIRM)} />
+      case CONFIRM:
+        return <Confirm
+          onConfirm={() => transition(EMPTY)}
+          onCancel={() => transition(SHOW)} />
       default:
         return <Empty />
     }
