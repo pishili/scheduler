6270d6034e1cdd7888b9666b45a4ee2e49a18b4b
ladan <pishili@users.noreply.github.com>
2020-03-20 10:26:42 -0700

    Fixes to handle varius number of spots on left side

--- src/components/Day/DayListItem.js
+++ src/components/Day/DayListItem.js
@@ -10,6 +10,12 @@ const formatSpots = (spots) => {
 		return "1 spot remaining";
 	} else if (spots === 2) {
 		return "2 spots remaining";
+	} else if (spots === 3) {
+		return "3 spots remaining";
+	} else if (spots === 4) {
+		return "4 spots remaining";
+	} else if (spots === 5) {
+		return "5 spots remaining";
 	} else {
 		return spots;
 	}
@@ -20,15 +26,17 @@ export default function DayListItem(props) {
 	const { name, selected, spots, setDay } = props;
 
 	let dayClass = classNames(["day-list__item",
-							  { "day-list__item--selected": selected,
-							    "day-list__item--full": spots === 0 }]);
-			
+		{
+			"day-list__item--selected": selected,
+			"day-list__item--full": spots === 0
+		}]);
+
 	return (
-	<li className={dayClass} onClick={() => setDay(name)}>
-	   <h2 >{name}</h2>
-	   <h3 className={dayClass}>{formatSpots(spots)}</h3>
-    </li>
-  );
+		<li className={dayClass} onClick={() => setDay(name)}>
+			<h2 >{name}</h2>
+			<h3 className={dayClass}>{formatSpots(spots)}</h3>
+		</li>
+	);
 }
 
 
